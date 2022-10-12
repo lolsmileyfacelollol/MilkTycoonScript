ad757fad7ad80ceac534109bdd421137f120d552
lolsmileyfacelollol <115596442+lolsmileyfacelollol@users.noreply.github.com>
2022-10-11 21:14:44 -0400

    Update MILKTYCOON.lua

--- MILKTYCOON.lua
+++ MILKTYCOON.lua
@@ -5,7 +5,7 @@ local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/Singularit
 local window = ui.new({text="Milk Tycoon - Cj#9089"})
 local mainTab = window.new({text="main"})
 local pickupMilk = mainTab.new("Button", {text="Pickup Milk Once"})
-local autoPickupToggle = mainTab.new("Switch", {text="Auto Pickup Milk"})
+--local autoPickupToggle = mainTab.new("Switch", {text="Auto Pickup Milk"})
 local flyToggle = mainTab.new("Switch", {text="fly"})
 local FlySpeedSlider = mainTab.new("Slider", {text="Fly Speed", min=1, max=99, value=1})
 local mouse = Players.LocalPlayer:GetMouse()
